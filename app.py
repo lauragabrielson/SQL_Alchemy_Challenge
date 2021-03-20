c866983c5d3053dd35783a4d2e4c4d27af40eafe
lauragabrielson <lauramwinge@hotmail.com>
2021-03-20 10:34:08 -0500

    Final

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def welcome():
         f"/api/v1.0/precipitation<br>"
         f"/api/v1.0/stations<br>"
         f"/api/v1.0/tobs<br>"
-        f"/api/v1.0/<start><br>"
-        f"/api/v1.0/<start>/<end><br>"
+        f"/api/v1.0/start<br>"
+        f"/api/v1.0/start/end<br>"
     )
 
 ###Start HERE:
@@ -55,7 +55,7 @@ def precipitation():
     """Return a list of precipitation for last 12 months"""
     # Query precipitatoin
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=366)
-    year_precp = session.query(Measurement.date, func.max(Measurement.prcp)).filter(func.strftime('%Y-%m-%d',Measurement.date) > year_ago).group_by(Measurement.date).all()
+    year_precp = session.query(Measurement.date, func.max(Measurement.prcp)).filter(func.strftime('%Y-%m-%d',Measurement.date) >= year_ago).group_by(Measurement.date).all()
     all_prcp = []
     for date, prcp in year_precp:
         precp_dict = {}
@@ -63,14 +63,12 @@ def precipitation():
         precp_dict["prcp"] = prcp
         all_prcp.append(precp_dict)
 
+    session.close()
     return jsonify(all_prcp)
    
 
-    # Convert list of tuples into normal list
-    #year_results = list(np.ravel(year_precp))
-
-    return jsonify(all_prcp)
-    session.close()
+  
+    
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -79,12 +77,16 @@ def stations():
 
     """Return a list of Stations"""
     # Query all Stations
-    stations = session.query(Station.station, Station.name, Station.elevation, Station.latitude, Station.longitude).all()
-
-
-    return jsonify(stations)
+    #stations = session.query(Station.station, Station.name, Station.elevation, Station.latitude, Station.longitude).all()
+    stations = session.query(Station.station).all()
+    all_names = list(np.ravel(stations))
+    
 
     session.close()
+ 
+    return jsonify(stations = all_names)
+
+    
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -95,34 +97,33 @@ def tobs():
     # Query all temps for last year
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=366)
     year_temps = session.query(Measurement.date, Measurement.tobs).filter((Measurement.station)=="USC00519281").filter(func.strftime('%Y-%m-%d',Measurement.date) > year_ago).group_by(Measurement.date).all()
-
-
+    
+    session.close()
     return jsonify(year_temps)
 
-    session.close()
-
-
-@app.route("/api/v1.0/<start><br>")
-def start():
+@app.route("/api/v1.0/<start>")
+def start(start):
     session = Session(engine)
-    start_date = ''
-    temps = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter((Measurement.date)>=start_date.all())
+ 
     
-    return(temps)
-
+    temps = session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(func.strftime('%Y-%m-%d',Measurement.date) >= start).group_by(Measurement.date).all()
+    
     session.close()
-
-@app.route("/api/v1.0/<start>/<end><br>")
-def start_end():
-    session = Session(engine)
-    start_date = ''
-    end_date = ''
-    temps = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter((Measurement.date>=start_date).filter(Measurement.date<= end_date).all())
-
     return jsonify(temps)
 
+    
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    session = Session(engine)
+
+    
+    temps = session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(func.strftime('%Y-%m-%d',Measurement.date) >= start).filter(func.strftime('%Y-%m-%d',Measurement.date) <= end).group_by(Measurement.date).all()
     session.close()
+    return jsonify(temps)
+
+    
     
    
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
